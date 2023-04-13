af9e9a52ea3256136d944ba422592d7584bec8e3
LaveWinter <lavinia_000@outlook.com>
2023-04-13 14:42:09 -0300

    Iniciei alguns métodos da classe GerenciadorRotas e GerenciadorAeronaves

--- MyFlight_base/src/pucrs/myflight/modelo/GerenciadorAeronaves.java
+++ MyFlight_base/src/pucrs/myflight/modelo/GerenciadorAeronaves.java
@@ -1,6 +1,33 @@
 package pucrs.myflight.modelo;
 
+import java.util.ArrayList;
+// Classe de Gerenciamento de aeronaves
+//Testes ainda por serem feitos
+// Manipula os dados da classe "Aeronave"
 public class GerenciadorAeronaves {
 
+    private ArrayList<Aeronave> aeronaves;
+
+// Construtor
+    public GerenciadorAeronaves(){
+        aeronaves = new ArrayList<>();
+    }
+
+//Adiciona aeronaves
+    public void adicionar(Aeronave aviao){
+        aeronaves.add(aviao);
+    }
+
+//Lista as aeronaves
+    private ArrayList<Aeronave> listarTodas(){
+        return aeronaves;
+        
+    }
+
+//Busca as aeronaves por código. Ainda não completa.
+    public Aeronave buscarPorCodigo(String cod){
+        return null;
+    }
+
 }
-//teste
+
--- MyFlight_base/src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ MyFlight_base/src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -1,5 +1,21 @@
 package pucrs.myflight.modelo;
 
+import java.util.ArrayList;
+
 public class GerenciadorRotas {
+
+    
+
+    public void adicionar(Rota rota){
+
+    }
+
+    private ArrayList<Rota> listarTodas(){
+        return null;
+    }
+
+    private ArrayList<Rota> buscarPorOrigem(Aeroporto orig){
+        return null;
+    }
 
 }
