5df6c1b1fec8b434faaf0bab5940f350b0a49639
Guilherme Barreto <guipoglia@gmail.com>
2023-04-16 14:59:24 -0300

    implementada a classe GerenciadorCias

--- MyFlight_base/src/pucrs/myflight/modelo/GerenciadorCias.java
+++ MyFlight_base/src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -5,7 +5,34 @@ import java.util.ArrayList;
 public class GerenciadorCias {
 	private ArrayList<CiaAerea> empresas;
 	
+	//construtor
 	public GerenciadorCias() {
 		empresas = new ArrayList<>();
+	}
+	//Adiciona CiaAerea
+	public void adicionar(CiaAerea cia){
+        empresas.add(cia);
+    }
+
+	//Lista todas as cias
+    public ArrayList<CiaAerea> listarTodas(){
+        return empresas;
+        
+    }
+
+	public CiaAerea buscarCodigo(String cod){
+		for (CiaAerea cia: empresas) {
+			if (cia.getCodigo() == cod)
+				return cia;
+		}
+		return null;
+	}
+
+	public CiaAerea buscarNome(String nome){
+		for (CiaAerea cia: empresas) {
+			if (cia.getCodigo() == nome)
+				return cia;
+		}
+		return null;
 	}
 }
