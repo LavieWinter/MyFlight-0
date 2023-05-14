ccdb8e73769f612e7649c6f25719f3d5b421e081
m.bortolossi <matheusbortolossi@gmail.com>
2023-05-14 13:01:51 -0300

    Update GerenciadorVoos.java
    Update da Classe gerenciado Voo sem a busca

--- MyFlight_base/src/pucrs/myflight/modelo/GerenciadorVoos.java
+++ MyFlight_base/src/pucrs/myflight/modelo/GerenciadorVoos.java
@@ -1,6 +1,26 @@
 package pucrs.myflight.modelo;
+import java.util.ArrayList;
 
 public class GerenciadorVoos {
 
+    private ArrayList<Voo> voos;
+
+    public GerenciadorVoos(){
+        voos = new ArrayList<>();
+    }
+
+    public void adicionar(Voo voo){
+        voos.add(voo);
+    }
+
+    public ArrayList<Voo> listarTodos(){
+        return voos;
+    }
+
+    /*
+    public Voo buscarPorData(data LocalDate){
+        
+    }
+     */
 }
-// Matheus
+
