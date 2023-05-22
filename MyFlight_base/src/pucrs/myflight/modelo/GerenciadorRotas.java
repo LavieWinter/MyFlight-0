d7b46bd3e5e2a275dc88ae588ec358a13cea6fe1
Lavínia Winter <105389999+LavieWinter@users.noreply.github.com>
2023-05-22 10:31:08 -0300

    att g.rotas

--- MyFlight_base/src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ MyFlight_base/src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -18,10 +18,13 @@ public class GerenciadorRotas {
         return rotas;
     }
 
-
-    //percebi que essa é meio complicada. vou pesquisar mais antes de fazer.
-    public ArrayList<Rota> buscarPorOrigem(Aeroporto orig){
-        return null;
-    }
+    public ArrayList<Rota> buscarCodigo(String orig){
+        ArrayList<Rota> resultado = new ArrayList<>();
+		for(int i = 0; i < rotas.size(); i++){
+			if (rotas.get(i).getOrigem().equals(orig))
+				resultado.add(rotas.get(i));
+		}
+		return resultado;
+	}
 
 }
