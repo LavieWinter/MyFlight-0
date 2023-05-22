47ed8c0109307bab77f6a399999dfef339c07b75
Lavínia Winter <105389999+LavieWinter@users.noreply.github.com>
2023-05-22 10:58:28 -0300

    att app e geo

--- MyFlight_base/src/pucrs/myflight/modelo/Geo.java
+++ MyFlight_base/src/pucrs/myflight/modelo/Geo.java
@@ -50,4 +50,22 @@ public class Geo {
                 
         return d;
 	} 
+
+	public double caldis(Geo geo2){
+		double lon1 = Math.toRadians(this.longitude);
+		double lat1 = Math.toRadians(this.latitude);
+		double lat2 = Math.toRadians(geo2.getLatitude());
+        double lon2 = Math.toRadians(geo2.getLongitude());
+        
+        double diflat = (lat1-lat2)/2;
+        double diflon = (lon1-lon2)/2;
+        
+        double d = Math.pow(Math.sin(diflat),2)+
+                   Math.pow(Math.sin(diflon),2)*
+                   Math.cos(lat1) * Math.cos(lat2);
+        
+        d = 2 * 6371 * Math.asin(Math.sqrt(d));
+		return d;
+
+	}
 }
