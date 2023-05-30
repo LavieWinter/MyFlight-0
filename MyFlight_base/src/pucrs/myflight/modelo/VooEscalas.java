aec2facbd092e1fb7276c162b0d37fba74b64d20
LaveWinter <lavinia_000@outlook.com>
2023-05-30 12:17:53 -0300

    att VooEscalas

--- MyFlight_base/src/pucrs/myflight/modelo/VooEscalas.java
+++ MyFlight_base/src/pucrs/myflight/modelo/VooEscalas.java
@@ -7,51 +7,20 @@ public class VooEscalas extends Voo {
     
     public enum Status { CONFIRMADO, ATRASADO, CANCELADO }; 
     
-    //private LocalDateTime datahora;
-    //private Duration duracao;
-    //private Rota rota;
     private Rota rotaFinal;
-    //private Status status;
 
     // Construtor
     public VooEscalas(Rota rota, Rota rotaFinal, LocalDateTime datahora, Duration duracao) {
         super(rota, datahora, duracao); // chama o construtor de Voo
         this.rotaFinal = rotaFinal;
      }
-   
-   /*  public VooEscalas(Rota rota, Rota rotaFinal, LocalDateTime dh, Duration dur) {
-    this.rota = rota;
-    this.rotaFinal = rotaFinal;
-    this.datahora = dh;
-    this.duracao = dur;
-    } */
 
-    /*public Rota getRota(){
-        return rota;
-    }*/
-
+    //getter 
     public Rota getRotaFinal(){
         return rotaFinal;
     }
 
-    /*public LocalDateTime getDataHora(){
-        return datahora;
-    }*/
-
-    /*public Duration getDuracao(){
-        return duracao;
-    }*/
-
-    /*@Override
-    public String toString() {
-       return status + " " + datahora + "("+duracao+"): " + rota + " -> " + rotaFinal;
-    }*/
-
-    /*@Override
-    public String toString() {
-       return getStatus() + " " + getDataHora() + "("+getDuracao()+"): " + getRota() + " -> " + rotaFinal;
-    }*/
-
+    //toString
     @Override
     public String toString() {
        return super.toString() + " -> " + rotaFinal;
--- MyFlight_base/src/pucrs/myflight/modelo/VooVariasEscalas.java
+++ MyFlight_base/src/pucrs/myflight/modelo/VooVariasEscalas.java
@@ -1,6 +1,16 @@
 package pucrs.myflight.modelo;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 // extends class Voo
-public class VooVariasEscalas {
+public class VooVariasEscalas extends VooEscalas{
+
+    private VooEscalas vooEscalas;
+
+    public VooVariasEscalas(Rota rota, Rota rotaFinal, LocalDateTime datahora, Duration duracao) {
+        super(rota, rotaFinal, datahora, duracao);
+    }
     
 
 }
